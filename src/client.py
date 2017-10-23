a7c6832c229607bf0bc9354fee107b32566fe494
Hewlette Dibble <hcodydibble@gmail.com>
2017-10-23 14:59:04 -0700

    we have working sockets!

--- src/client.py
+++ src/client.py
@@ -4,6 +4,14 @@ import socket
 
 def client(message):
     """Will do a thing."""
-    client_socket = socket.socket(*socket.getaddrinfo("127.0.0.1", 3455)[1][:3])
-    client_socket.connect(("127.0.0.1", 3455))
-    client_socket.sendall(message.encode("utf8"))
+    client = socket.socket(*socket.getaddrinfo("127.0.0.1", 3455)[1][:3])
+    client.connect(("127.0.0.1", 3455))
+    client.sendall(message.encode("utf8"))
+    msg = ''
+    while True:
+        port = client.recv(8)
+        msg += port.decode("utf8")
+        if len(port) < 8:
+            break
+    print(msg)
+    client.close()
--- src/server.py
+++ src/server.py
@@ -1,15 +1,20 @@
+"""."""
 import socket
 
 
 def server():
-    """This does stuff"""
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPORT_TCP)
+    """Will do stuff."""
+    server = socket.socket(socket.AF_INET,
+                           socket.SOCK_STREAM, socket.IPPROTO_TCP)
     server.bind(('127.0.0.1', 3455))
     server.listen(1)
     conn, addr = server.accept()
+    msg = ''
     while True:
         port = conn.recv(8)
-        msg += port
+        msg += port.decode("utf8")
         if len(port) < 8:
             break
-    conn.sendall(msg.encode('utf8'))
+    conn.sendall(msg.encode("utf8"))
+    conn.close()
+    server.close()
