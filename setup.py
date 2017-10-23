14851f376351e022e90dd376ba79b502d19f84ac
Hewlette Dibble <hcodydibble@gmail.com>
2017-10-23 14:17:48 -0700

    added some things to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='Sockets',
+    name='http-server',
     description='''Sets up server and client sockets''',
     author='Cody Dibble, Chai Narukulla and Philip Werner',
     package_dir={'': 'src'},
-    py_modules=['Sockets'],
+    py_modules=['client', 'server'],
     install_requires=[],
     extras_require={
-        'testing': ['pytest', 'pytest-cov'],
+        'testing': ['pytest', 'pytest-cov', 'tox'],
         'development': ['ipython']},
     entry_points={
         'console_scripts': [
