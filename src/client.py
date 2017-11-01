bf3fba74bfa0a0c529d4b7d4715f0c384bfd6e2e
Hewlette Dibble <hcodydibble@gmail.com>
2017-11-01 08:31:05 -0700

    fixed some issues

--- src/client.py
+++ src/client.py
@@ -6,25 +6,25 @@ import sys
 
 def client(message):
     """Open a client to send messages."""
-    client = socket.socket(*socket.getaddrinfo("127.0.0.1", 5000)[1][:3])
-    client.connect(("127.0.0.1", 5000))
+    client = socket.socket(*socket.getaddrinfo("127.0.0.1", 5001)[1][:3])
+    client.connect(("127.0.0.1", 5001))
     request_header = "GET /http-server/src/server.py HTTP/1.1\r\n\
-    HOST: 127.0.0.1:5000"
+    HOST: 127.0.0.1:5001"
     message = request_header + message + "@@@"
     if sys.version_info.major == 3:
         client.sendall(message.encode("utf-8"))
     else:
         client.sendall(message)
     msg = b''
-    timer = True
-    while timer:
+    message_incomplete = True
+    while message_incomplete:
         part = client.recv(15)
         msg += part
         if b"@@@" in msg:
-            timer = False
+            message_incomplete = False
     client.close()
     return msg.decode("utf-8").replace("@@@", "")
 
-if __name__ is "__main__":  # pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
     msg = sys.argv[1]
     print(client(msg))
--- src/server.py
+++ src/server.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """Open a server and listen for messages from the client."""
 import socket
-import sys
 import email.utils
 
 
@@ -9,25 +8,25 @@ def server():  # pragma: no cover
     """Open a server to echo back a message."""
     server = socket.socket(socket.AF_INET,
                            socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    server.bind(('127.0.0.1', 5000))
+    server.bind(('127.0.0.1', 5001))
     server.listen(1)
     try:
         while True:
             conn, addr = server.accept()
             msg = b''
-            timer = True
-            while timer:
+            message_incomplete = True
+            while message_incomplete:
                 part = conn.recv(8)
                 msg += part
                 if b"@@@" in msg:
-                    timer = False
-            print(msg.decode("utf-8"))
+                    message_incomplete = False
+            print(msg.decode("utf-8").replace("@@@", ""))
+            conn.sendall(response_ok() + msg)
             conn.close()
     except KeyboardInterrupt:
         conn.close()
         server.close()
         print("\nClosing the server!")
-        sys.exit(1)
 
 
 def response_ok():
@@ -40,11 +39,11 @@ def response_ok():
 
 def response_error():
     """Return a HTTP "500 Internal Server Error"."""
-    return "500 Internal Server Error: The server encountered an internal error\
+    return "HTTP/1.1 500 Internal Server Error: The server encountered an internal error\
     or misconfiguration and was unable to complete your request.\
     Please contact the server admin, master_of_web@example.com, and inform\
     them of the time the error occured, and anything you might have done that\
     may have caused the error."
 
-if __name__ is "__main__":  # pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
     server()
